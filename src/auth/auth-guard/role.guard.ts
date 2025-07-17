9ebf08ebf28ae77e2c3b21e3a096abdb74ff8e92
Akshata <teggiakshata2002@gmail.com>
2025-07-17 15:08:46 +0530

    .

--- src/auth/auth-guard/role.guard.ts
+++ src/auth/auth-guard/role.guard.ts
@@ -21,57 +21,20 @@
 
 
 
-// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-// import { Reflector } from '@nestjs/core';
-
-// @Injectable()
-// export class RolesGuard implements CanActivate {
-//   constructor(private readonly reflector: Reflector) {}
-
-//   canActivate(context: ExecutionContext): boolean {
-//     const requiredRoles = this.reflector.get<string[]>('roles', context.getHandler());
-//     if (!requiredRoles) return true;
-
-//     const request = context.switchToHttp().getRequest();
-//     const user = request.user;
-
-//     return requiredRoles.includes(user.roleId);
-//   }
-// }
-
-
-
-
-
-
-
-
-
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  ForbiddenException,
-} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from '../decorator/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
-      ROLES_KEY,
-      [context.getHandler(), context.getClass()],
-    );
+    const requiredRoles = this.reflector.get<string[]>('roles', context.getHandler());
     if (!requiredRoles) return true;
 
-    const { user } = context.switchToHttp().getRequest();
-    if (!user?.roleId) {
-      throw new ForbiddenException('No role assigned to user');
-    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
 
-    return requiredRoles.includes(user.roleId); // or user.role.name if available
+    return requiredRoles.includes(user.roleId);
   }
 }
